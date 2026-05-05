be0d7c86a7ebc6702ba89837b7925b165c6a39dc
Rayene <mohamed.romdhane@etu.univ-cotedazur.fr>
2026-05-05 22:57:17 +0200

    fix: server lint

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,9 +7,7 @@ import { RateLimiter } from './lib/rateLimit';
 import { bindSocket } from './lib/socketHandlers';
 
 const IS_DEV = process.env.NODE_ENV !== 'production';
-const DEFAULT_CORS_ORIGIN = IS_DEV
-  ? 'http://localhost:3000'
-  : 'https://chkobagame.xyz';
+const DEFAULT_CORS_ORIGIN = IS_DEV ? 'http://localhost:3000' : 'https://chkobagame.xyz';
 const ALLOWED_ORIGINS = new Set([
   'https://chkobagame.xyz',
   'http://localhost:3000',
